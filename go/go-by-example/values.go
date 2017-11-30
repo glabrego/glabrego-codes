f6ae3dfdfca2c34dae37dd4e95d5ece7128b1aba
Guilherme Labrego <glabrego@gmail.com>
2017-11-30 20:59:05 -0200

    Add Go values studies

---
+++ go/go-by-example/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Guilherme" + "Labrego")
+
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("23.0 / 3.0 = ", 27.0/3.0)
+
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println(true || false)
+	fmt.Println(true && false)
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
