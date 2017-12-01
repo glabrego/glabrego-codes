97211a84ceeabd5cd79ff663bf49ad6f4b15136e
Guilherme Labrego <glabrego@gmail.com>
2017-12-01 21:08:09 -0200

    Add knowledge about variables in Go

---
+++ go/go-by-example/variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+  var initial string
+  var num_initial int
+  var a, b string = "Guilherme", "Labrego"
+  var num int = 1
+  var flo = 2.1
+  test := true
+
+  fmt.Println(initial)
+  fmt.Println(num_initial)
+
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(a, b)
+  fmt.Println(a + b)
+
+  fmt.Println(num)
+  fmt.Println(flo)
+
+  fmt.Println(test)
+}
