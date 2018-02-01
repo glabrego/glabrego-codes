5359497ded8685daf4440f2189e121a301fdff96
Guilherme Labrego <glabrego@gmail.com>
2018-01-31 22:03:27 -0200

    Add knowledge about how to close channels in Go

---
+++ go/go-by-example/closing-channels.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("Received job:", j)
+			} else {
+				fmt.Println("Received all jobs available")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("Sent job", j)
+	}
+	close(jobs)
+	fmt.Println("Sent all jobs")
+
+	<-done
+}
