cc7e2731ee854d63728dc42dca27fc36b003b742
Guilherme Labrego <glabrego@gmail.com>
2018-02-07 18:03:15 -0200

    Add knowledge about functions collections in Go

---
+++ go/go-by-example/collection-functions.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Index(vs []string, target string) int {
+	for i, v := range vs {
+		if v == target {
+			return i
+		}
+	}
+	return -1
+}
+
+func Include(vs []string, target string) bool {
+	return Index(vs, target) >= 0
+}
+
+func Any(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func All(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+func Map(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+func main() {
+	var strs = []string{"peach", "apple", "pear", "plum"}
+
+	fmt.Println(Index(strs, "pear"))
+	fmt.Println(Include(strs, "grape"))
+	fmt.Println(Any(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+
+	fmt.Println(All(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+
+	fmt.Println(Filter(strs, func(v string) bool {
+		return strings.Contains(v, "e")
+	}))
+
+	fmt.Println(Map(strs, strings.ToUpper))
+}
